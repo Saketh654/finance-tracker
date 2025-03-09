2bc7e493b8b0d45faf310c4b17dcbc7d5ad9d8c5
Nagasaketh <naga.saketh654@gmail.com>
2025-03-09 22:55:16 +0530

    Added transactions page with add transaction feature

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider, updateProfile } from "firebase/auth";
 import { getFirestore, collection, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcu961I8u1SThZSK5b2i_CKqJH0pCBSE8",
-  authDomain: "finance-tracker-48c98.firebaseapp.com",
-  projectId: "finance-tracker-48c98",
-  storageBucket: "finance-tracker-48c98.firebasestorage.app",
-  messagingSenderId: "981555263193",
-  appId: "1:981555263193:web:8e58cac643f4e4ceba30c8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 
